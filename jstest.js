a9af6c10eb415ec8bda7a49fb0b494e2903e22e0
Shiva Mishra <111176691+ShivaaMishra@users.noreply.github.com>
2022-09-01 18:14:26 -1100

    Add files via upload

---
+++ jstest.js
@@ -0,0 +1,18 @@
+ var valueList = document.getElementById('valueList');
+ var text = '<span> you have selected : </span>';
+ var listArray = [];
+
+ var checkboxes = document.querySelectorAll('.checkbox');
+
+ for(var checkbox of checkboxes){
+    checkbox.addEventListener('click', function(){
+        if(this.checked == true){
+            listArray.push(this.value);
+            valueList.innerHTML = listArray.join(' <br> ');
+        }else{
+            listArray = listArray.filter(e => e !== this.value);
+            valueList.innerHTML = listArray.join(' <br> ');
+        }
+    })
+ }
+
